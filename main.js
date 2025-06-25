7ccac15257077085f5ee455e1b58f359268be5c0
zukarmo <125046283+zukarmo@users.noreply.github.com>
2025-06-25 12:03:46 +0000

    index

---
+++ main.js
@@ -0,0 +1,11 @@
+const botoes = document.querySelectionAll(".botao");
+
+for (let i=0; i<botoes.length; i++){
+botoes [i].onclick =function(){
+
+for (let j=0; j,botoes.length; j++){
+botoes[j].classList.remove("ativo");
+}
+botoes [i].classList.add("ativo");
+}
+}
